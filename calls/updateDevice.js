d8dbe3cf14e5aa73a35a8786062f3b75738608e7
dsanchezderivera <diego.sanchezderiveracordoba@gmail.com>
2018-09-21 12:54:35 +0000

    pass MAC to updateProc

--- calls/updateDevice.js
+++ calls/updateDevice.js
@@ -1,12 +1,13 @@
 const util = require('util');
 const { execFile } = require('child_process');
+//Config file
 var config = require('../config.js').get(process.env.NODE_ENV);
 
-//Scan BT Devices calling external method and waiting execution
+//Update specific BT Device calling external method and waiting execution
 function update(mac, image) {
     return new Promise(function(resolve, reject) {
     	//Dummy-Printing node version
-        execFile(config.updatepath, [], (error, stdout, stderr) => {
+        execFile(config.updatepath, [mac], (error, stdout, stderr) => {
 			if (error) reject(error);
 		 	resolve(stdout);
 		});
